affda8165aabf7defc2d6958bcae833fa33553c8
Caue Garcia <cauerg7@outlook.com>
2024-09-21 16:19:52 -0300

    Search products by name ok

--- dscommerce-restassured/dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ dscommerce-restassured/dscommerce-restassured/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscommerce.controllers;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,10 +12,13 @@ import org.junit.jupiter.api.Test;
 public class ProductControllerRA {
 
 	private Long existingProductId, nonExistingProductId;
+	private String productName;
 	
 	@BeforeEach
 	public void setUp() {
 		baseURI = "http://localhost:8080";
+		
+		productName = "Macbook";
 	}
 	
 	@Test
@@ -31,4 +36,44 @@ public class ProductControllerRA {
 			.body("categories.id", hasItems(2, 3))
 			.body("categories.name", hasItems("Eletrônicos", "Computadores"));
 	}
+	
+	@Test
+	public void findAllShouldReturnPageProductWhenProductNameIsEmpty() {
+			given()
+			.get("/products?page=0")
+		.then()
+			.statusCode(200)
+			.body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPageProductsWhenProductNameIsNotEmpty() {
+			given()
+			.get("/products?name={productName}", productName)
+		.then()
+			.statusCode(200)
+			.body("content.id[0]", is(3))
+			.body("content.name[0]", equalTo("Macbook Pro"))
+			.body("content.price[0]", is(1250.0F))
+			.body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"));
+	}
+	
+	@Test
+	public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() {
+			given()
+			.get("/products?size=25")
+		.then()
+			.statusCode(200)
+			.body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
